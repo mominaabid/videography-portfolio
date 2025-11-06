cf84cb1a9f0650cbf972493eae8d3ac461d8cfe1
hamna <catto4561@gmail.com>
2025-11-07 01:33:14 +0500

    styling and navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Portfolio from './Pages/Portfolio';
